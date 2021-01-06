f4fb05d26c6e1eb54e8039849896b5f400730a45
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2021-01-07 03:25:13 +0500

    Flux Reactor

--- src/test/java/ReactiveJavaTest.java
+++ src/test/java/ReactiveJavaTest.java
@@ -1,6 +1,12 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Random;
 
 public class ReactiveJavaTest {
 
@@ -62,6 +68,81 @@ public class ReactiveJavaTest {
                 .onErrorReturn("A")
                 .log()
                 .subscribe(System.out::println);
+    }
+
+
+    @Test
+    public void fluxJustTest() {
+        String [] test = {"A","B","C"};
+        Flux.just(test)
+                .map(String::toLowerCase)
+                .log()
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void fluxFromIterator() {
+        Flux.just(Arrays.asList("A","B","C"))
+                .log().subscribe();
+
+        Flux.fromIterable(Arrays.asList("A","B","C"))
+                .map(String::toLowerCase)
+                .log()
+                .subscribe();
+    }
+
+    @Test
+    public void fluxFromRange() {
+        Flux.range(8, 10)
+                .log().subscribe();
+    }
+
+
+    //Take method does not work as backpressure
+    // it just take N values and terminate process
+    @Test
+    public void fluxRangeAndTake() throws InterruptedException {
+        Flux.interval(Duration.ofSeconds(1))
+        .log()
+        .take(3)
+        .subscribe();
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void fluxBackPressure() {
+        Flux.range(3, 10)
+                .log()
+                .subscribe(null, null, null,
+                        s->s.request(3));
+    }
+
+    @Test
+    public void fluxWithComplexBackPressure() {
+        Flux.range(3, 10)
+        .log()
+        .subscribe(new BaseSubscriber<Integer>() {
+            int elementsToProcess = 3;
+            int count = 0;
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(elementsToProcess);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                super.hookOnNext(value);
+                System.out.println(value);
+                count++;
+                if (count == elementsToProcess) {
+                    count = 0;
+
+                    Random r = new Random();
+                    elementsToProcess = r.ints(1, 4).findFirst().getAsInt();
+                    request(elementsToProcess);
+                }
+            }
+        });
     }
 
 }
